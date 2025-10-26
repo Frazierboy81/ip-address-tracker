5d343a5bfa6d86a9a6194d837c1cd6db6d39bbd5
Frazier <mrfrazier628@hotmail.com>
2025-10-26 02:53:40 -0400

    ipaddress update

--- src/script.js
+++ src/script.js
@@ -8,44 +8,19 @@ const timeZone = document.getElementById("time-zone")
 const intntSvc = document.getElementById("internet-service")
 const form = document.querySelector("form");
 
+
 const map = L.map(mapDiv)
 
 function mapData(data) {
   ipSearch.textContent = `IP Adress: ${data.location.ipSearch}`
   location.textContent = `Location: ${data.location.region}, ${data.location.city}, ${data.location.postalCode}`
   timeZone.textContent = `UTC: ${data.location.timezone}`
+  intntSvc.textContent = `ISP: ${data.location.intntSvc}`
 }
 
-// async function showMapData() {
-//   try {
-//     const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${addressIp}`)
-//   if (!res.ok) {
-//     throw new Error ("Enter a valid IP address!")
-//     }
-//     const data = await res.json();
-//     console.log(data);
-//     mapData(data)
-
-//     map.setView(
-//       [data.location.lat, data.location.lng],
-//       13);
-
-//     const marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
-//     marker.bindPopup("I'm here!!").openPopup();
-
-//   } catch (error) {
-//     console.error(error);
-//     }
-// }
-//  showMapData()
-
-
-
-form.addEventListener("submit", async (event) => {
-event.preventDefault();
+async function showMapData(event) {
+  event.preventDefault();
   const addressIp = ipSearch.value;
-  // console.log(addressIp)
-    
   try {
     const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${addressIp}`)
   if (!res.ok) {
@@ -65,10 +40,14 @@ event.preventDefault();
   } catch (error) {
     console.error(error);
     }
-  })
+}
 
 
-document.addEventListener("DOMContentLoaded", async () => {
+form.addEventListener("submit", showMapData());
+
+
+
+// document.addEventListener("DOMContentLoaded", async () => {
   try {
     const res = await fetch(
       `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`
